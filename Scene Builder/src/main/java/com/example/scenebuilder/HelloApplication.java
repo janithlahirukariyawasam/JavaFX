f633e232616e6273341e6b7a0bc0604103993b8f
DESKTOP-MF5FVPM\Lenovo <kariyaw-im19081@stu.kln.ac.lk>
2021-12-24 19:55:36 +0530

    Scene Builder training

---
+++ Scene Builder/src/main/java/com/example/scenebuilder/HelloApplication.java
@@ -0,0 +1,25 @@
+package com.example.scenebuilder;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Scene Builder samples");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
