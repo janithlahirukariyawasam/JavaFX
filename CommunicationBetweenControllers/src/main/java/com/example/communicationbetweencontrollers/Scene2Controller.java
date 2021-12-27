f17cd02d3ca682cbd3dd8aeeadc6bfa46d0b74f7
DESKTOP-MF5FVPM\Lenovo <kariyaw-im19081@stu.kln.ac.lk>
2021-12-27 18:09:51 +0530

    Finished

---
+++ CommunicationBetweenControllers/src/main/java/com/example/communicationbetweencontrollers/Scene2Controller.java
@@ -0,0 +1,15 @@
+package com.example.communicationbetweencontrollers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class Scene2Controller {
+
+    @FXML
+    Label nameLabel;
+
+    public void displayName(String username) {
+        nameLabel.setText("Hello: " + username);
+    }
+
+}
