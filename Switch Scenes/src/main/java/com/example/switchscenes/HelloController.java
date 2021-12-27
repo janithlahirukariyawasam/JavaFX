5dedbc04617fb65457f962075af6e6d8cd050f2e
DESKTOP-MF5FVPM\Lenovo <kariyaw-im19081@stu.kln.ac.lk>
2021-12-27 10:49:17 +0530

    12/27

--- Switch Scenes/src/main/java/com/example/switchscenes/HelloController.java
+++ Switch Scenes/src/main/java/com/example/switchscenes/HelloController.java
@@ -1,8 +1,34 @@
 package com.example.switchscenes;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
 
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToScene1(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchToScene2(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("hello.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
